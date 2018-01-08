ac052becb3d6a26aed6ac319f3071de1b0375b8a
ryan.lewkowicz@ge.com <ryan.lewkowicz@ge.com>
2018-01-08 07:11:57 -0600

    Begrudgenly doing some book work

---
+++ cf/cf.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"gopl.io/ch2/tempconv"
+)
+
+func main() {
+	for _, arg := range os.Args[1:] {
+		t, err := strconv.ParseFloat(arg, 64)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+			os.Exit(1)
+		}
+		f := tempconv.Fahrenheight
+	}
+}
